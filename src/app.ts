e336fbb3a1df47c0029f2e85bd996811ed4198c5
Vivek Singh <vivek.singh@liqvid.com>
2023-05-07 23:20:07 +0530

    cors changes

--- src/app.ts
+++ src/app.ts
@@ -27,13 +27,13 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-// app.use(cors({
-//   origin: "http://localhost:3000",
-//   methods: "GET,POST,PUT,DELETE",
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: ["http://localhost:3000","http://www.feedbacksense.tech","https://www.feedbacksense.tech"],
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true,
+}));
 
-app.use(cors());
+// app.use(cors());
 
 app.use(
   cookieSession({
