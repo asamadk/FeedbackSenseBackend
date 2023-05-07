ff3244e1d61415a29418d92af6f3a2e89b66a930
Vivek Singh <vivek.singh@liqvid.com>
2023-05-07 18:39:51 +0530

    Enable all cors

--- src/app.ts
+++ src/app.ts
@@ -27,11 +27,13 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors({
-  origin: "http://localhost:3000",
-  methods: "GET,POST,PUT,DELETE",
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: "http://localhost:3000",
+//   methods: "GET,POST,PUT,DELETE",
+//   credentials: true,
+// }));
+
+app.use(cors());
 
 app.use(
   cookieSession({
